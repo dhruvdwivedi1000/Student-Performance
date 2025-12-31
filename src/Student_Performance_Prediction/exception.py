25637cf0d93ac3c005096e64fba6505074f6003c
Dhruv <dhruvdwivedi1000@gmail.com>
2025-12-31 15:50:03 +0530

    testing logging and exception

--- src/Student_Performance_Prediction/exception.py
+++ src/Student_Performance_Prediction/exception.py
@@ -12,7 +12,7 @@ def get_detailed_error_message(error_message: Exception, error_detail: sys) -> s
 class CustomException(Exception):
     def __init__(self, error_message: Exception, error_detail: sys):
         super().__init__(error_message)
-        self.error_message = self.get_detailed_error_message(error_message, error_detail)
+        self.error_message = get_detailed_error_message(error_message, error_detail)
     
     def __str__(self):
         return self.error_message
