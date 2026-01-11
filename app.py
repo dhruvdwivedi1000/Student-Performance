4482cc5624d5c8649b4cdf95de62dc522bd2f1fc
Dhruv <dhruvdwivedi1000@gmail.com>
2026-01-11 15:44:16 +0530

    model training and hyperparameter

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import sys
 from src.Student_Performance_Prediction.components.data_ingestion import DataIngestion
 from src.Student_Performance_Prediction.components.data_ingestion import DataIngestionConfig
 from src.Student_Performance_Prediction.components.data_transformation import DataTransformation
+from src.Student_Performance_Prediction.components.model_trainer import ModelTrainer, ModelTrainerConfig
 
 if __name__ == "__main__":
     logging.info("Application started")
@@ -15,7 +16,11 @@ try:
     data_ingestion = DataIngestion()
     train_data_path, test_data_path = data_ingestion.initiate_data_ingestion()
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transormation(train_data_path, test_data_path)
+    train_arr,test_arr,_=data_transformation.initiate_data_transormation(train_data_path,test_data_path)
+    
+    #Model Trainer
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
 
    
 except Exception as e:
